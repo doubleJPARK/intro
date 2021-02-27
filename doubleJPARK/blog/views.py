5526cf0cc8ba4f95494aaf7c96d584ea66927dc1
doubleJPARK <jinjongpark@naver.com>
2021-02-27 14:47:24 +0000

    CBV와 우당탕탕 블로그 수정기

--- doubleJPARK/blog/views.py
+++ doubleJPARK/blog/views.py
@@ -1,26 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
+class PostDetail(DetailView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts': posts
-        }
-    )
+# FBV 스타일 코드임 아마 앞으로 안 쓸것 같음
+# def index(request):
+#    posts = Post.objects.all().order_by('-pk')
+#
+#    return render(
+#        request,
+#        'blog/index.html',
+#        {
+#            'posts': posts
+#        }
+#    )
 
 
-def single_post_page(request, pk):
-    post = Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post': post,
-        }
-    )
+# def single_post_page(request, pk):
+#    post = Post.objects.get(pk=pk)
+#
+#    return render(
+#        request,
+#        'blog/single_post_page.html',
+#        {
+#            'post': post,
+#        }
+#    )
