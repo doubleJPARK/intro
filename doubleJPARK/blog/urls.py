0c1b6590dcd7b8c38d253dd0f3ca477a672c13b3
doubleJPARK <jinjongpark@naver.com>
2021-02-28 14:41:46 +0000

    기초 에러 수정

--- doubleJPARK/blog/urls.py
+++ doubleJPARK/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 from . import views
+from django.urls import include
 
 urlpatterns = [
+    path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    path('/<int:pk>/', views.PostDetail.as_view()),
-    # path('<int:pk>/', views.single_post_page),
-    # path('', views.index)
+    # path('<index:pk>/', views.single_post_page),
+    # path('', views.index),
 ]
