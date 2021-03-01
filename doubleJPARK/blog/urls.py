6aed5cd342a5fd3f4dba899f129c7333566a7000
doubleJPARK <jinjongpark@naver.com>
2021-03-01 10:12:14 +0000

    add bootstrap on post_list

--- doubleJPARK/blog/urls.py
+++ doubleJPARK/blog/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.urls import include
 
 urlpatterns = [
-    path('<int:pk>/', views.PostDetail.as_view()),
+    path('/<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<index:pk>/', views.single_post_page),
     # path('', views.index),
