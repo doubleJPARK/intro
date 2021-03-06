f0611fdfa94a901cf0565f03129b90861cada283
doubleJPARK <jinjongpark@naver.com>
2021-03-06 14:20:59 +0000

    make category page

--- doubleJPARK/blog/urls.py
+++ doubleJPARK/blog/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.urls import include
 
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
     # path('<index:pk>/', views.single_post_page),
--- doubleJPARK/blog/views.py
+++ doubleJPARK/blog/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Post, Category
 
@@ -20,6 +21,26 @@ class PostDetail(DetailView):
         context['categories'] = Category.objects.all()
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        post_list = Post.objects.filter(category=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        post_list = Post.objects.filter(category=category)
+
+    return render(
+        request,
+        'blog/post_list.html',
+        {
+            'post_list': post_list,
+            'categories': Category.objects.all(),
+            'no_category_post_count': Post.objects.filter(category=None).count(),
+            'category': category,
+        }
+    )
 
 # FBV 스타일 코드임 아마 앞으로 안 쓸것 같음
 # def index(request):
