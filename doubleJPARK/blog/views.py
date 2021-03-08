d9e15dd594fb16ac7a8dfb4852ef48f8211c2a60
doubleJPARK <jinjongpark@naver.com>
2021-03-08 13:58:51 +0000

    add post updating

--- doubleJPARK/blog/views.py
+++ doubleJPARK/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -38,6 +39,19 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
             return super(PostCreate, self).form_valid(form)
         else:
             return redirect('/blog/')
+
+
+class PostUpdate(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'hook_text','content','head_image','file_upload','category', 'tags']
+    
+    template_name = 'blog/post_update.html'
+    
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else:
+            return PermissionDenied
 
 
 def category_page(request, slug):
