57d23c0cea6d641abdf1e4eec987374094b7eb24
doubleJPARK <jinjongpark@naver.com>
2021-03-09 13:39:53 +0000

    add tag with text while writing post

--- doubleJPARK/blog/views.py
+++ doubleJPARK/blog/views.py
@@ -1,13 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post, Category, Tag
 from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
 
 class PostList(ListView):
     model = Post
     ordering = '-pk'
-    
+
     def get_context_data(self, **kwargs):
         context = super(PostList, self).get_context_data()
         context['categories'] = Category.objects.all()
@@ -17,7 +18,7 @@ class PostList(ListView):
 
 class PostDetail(DetailView):
     model = Post
-    
+
     def get_context_data(self, **kwargs):
         context = super(PostDetail, self).get_context_data()
         context['categories'] = Category.objects.all()
@@ -28,15 +29,33 @@ class PostDetail(DetailView):
 class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text','content','head_image','file_upload','category']
-    
+
     def test_func(self):
         return self.request.user.is_superuser or self.request.user.is_staff
-    
+
     def form_valid(self, form):
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
-            return super(PostCreate, self).form_valid(form)
+            response = super(PostCreate, self).form_valid(form)
+
+            tags_str = self.request.POST.get('tags_str')
+            if tags_str:
+                tags_str = tags_str.strip()
+
+                tags_str = tags_str.replace(',',';')
+                tags_list = tags_str.split(';')
+
+                for t in tags_list:
+                    t = t.strip()
+                    tag, is_tag_created = Tag.objects.get_or_create(name=t)
+                    if is_tag_created:
+                        tag.slug = slugify(t, allow_unicode=True)
+                        tag.save()
+                    self.object.tags.add(tag)
+
+            return response
+
         else:
             return redirect('/blog/')
 
@@ -46,7 +65,7 @@ class PostUpdate(LoginRequiredMixin, CreateView):
     fields = ['title', 'hook_text','content','head_image','file_upload','category', 'tags']
     
     template_name = 'blog/post_update.html'
-    
+
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated and request.user == self.get_object().author:
             return super(PostUpdate, self).dispatch(request, *args, **kwargs)
